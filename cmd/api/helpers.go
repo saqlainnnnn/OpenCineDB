3218261914e3f29d904c3fd80c47959c1872476c
saqlain <saqlainsibtain123@gmail.com>
2024-09-08 13:51:37 +0530

    added writeJson function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,14 +9,34 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+func (app *application) writeJson(w http.ResponseWriter, status int, data interface{}, headers http.Header ) error {
+	// marrshal encodes the data into json
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// makes it easier to read 
+	js = append(js, '\n')
+	// the headers are added to the http response headers
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	//adding json to the header, then writing status code and json response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
+
+
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10 , 64)
 
-	if err != nil || id <1 {
+	if err != nil || id <1 { 
 		return 0, errors.New("invalid id parameter")
-
 	}
 
 	return id, nil
