8673aaeaff4177c9158370730a48a2b4f2eafdcd
saqlain <saqlainsibtain123@gmail.com>
2024-09-12 15:28:22 +0530

    added validation checks

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"greelight.alexedwards.net/internal/data"
+	"greelight.alexedwards.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +21,29 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title)<= 5000, "title", "must not be more than 5000 words" )
+
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "year must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "year must be greater than 1888")
+
+	v.Check(input.Runtime !=0, "rutime", "must be provided")
+	v.Check(input.Runtime >0, "rutime", "must be a positive integer")
+
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >=1, "genres", "must contain at least 1 genre")
+	v.Check(len(input.Genres) <= 5, "genres", "must be less than 5 genre")
+
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	if !v.Valid() {
+		app.failedValidResponse(w, r, v.Errors)
 		return
 	}
 
