96e9b999c69d97f861c39be700d370cc9af0a915
saqlain <saqlainsibtain123@gmail.com>
2024-09-15 19:09:38 +0530

    added get fucntion functionality

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"greelight.alexedwards.net/internal/data"
 	"greelight.alexedwards.net/internal/validator"
@@ -38,7 +38,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return 
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
+
+	err = app.writeJson(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app*application) showMovieHandler(w http.ResponseWriter, r *http.Request)  {
@@ -48,15 +60,18 @@ func (app*application) showMovieHandler(w http.ResponseWriter, r *http.Request) 
 		app.notFoundResponse(w,r)
 		return
 	}
-	//created instance of movie struct
-	movie := data.Movie {
-		ID: id,
-		CreatedAt: time.Now(),
-		Title: "Casablanca",
-		Runtime: 102,
-		Genres: []string{"drama", "romance", "war"},
-		Version: 1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w,r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
+
+
 	//this time encoding a struct
 	err = app.writeJson(w, http.StatusOK, envelope{"movie" : movie}, nil)
 	if err != nil {
